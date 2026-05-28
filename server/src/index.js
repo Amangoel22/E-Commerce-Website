d7e374f9e3f3227968f0f5e55de8202a30a423a0
amangoel <aman.goelmg@gmail.com>
2026-05-28 13:10:42 +0530

    Production changes

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,13 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://shop-com-frontend.onrender.com'
+  ],
+  credentials: true
+}))
 app.use(express.json());
 
 const authRoutes = require('./routes/auth.route');
